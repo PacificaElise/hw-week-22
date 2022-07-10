7566b5086683f2debf199b74b8cc7755d61677d5
Tatiana Ivanova <tati.ivanova.87@gmail.com>
2022-07-10 17:07:08 +0300

    add styles

--- Dashboard/src/script.js
+++ Dashboard/src/script.js
@@ -1,14 +1,14 @@
 import { Chart, registerables } from 'chart.js';
 Chart.register(...registerables);
 
-const ctx = document.getElementById('myChart');
-const myChart = new Chart(ctx, {
+const ctx = document.getElementById('barChart');
+const barChart = new Chart(ctx, {
     type: 'bar',
     data: {
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [6, 10, 12, 13, 17, 21],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
@@ -37,3 +37,71 @@ const myChart = new Chart(ctx, {
     }
 });
 
+const ctx2 = document.getElementById('roundChart');
+const roundChart = new Chart(ctx2, {
+    type: 'polarArea',
+    data: {
+        labels: [
+            'Red',
+            'Green',
+            'Yellow',
+            'Grey',
+            'Blue'
+          ],
+          datasets: [{
+            label: 'My First Dataset',
+            data: [11, 16, 7, 3, 14],
+            backgroundColor: [
+              'rgb(255, 99, 132)',
+              'rgb(75, 192, 192)',
+              'rgb(255, 205, 86)',
+              'rgb(201, 203, 207)',
+              'rgb(54, 162, 235)'
+            ]
+          }]
+        },
+});
+
+const ctx3 = document.getElementById('radarChart');
+const radarChart = new Chart(ctx3, {
+    type: 'radar',
+    data: {
+        labels: [
+          'Eating',
+          'Drinking',
+          'Sleeping',
+          'Designing',
+          'Coding',
+          'Cycling',
+          'Running'
+        ],
+        datasets: [{
+          label: 'My First Dataset',
+          data: [65, 59, 90, 81, 56, 55, 40],
+          fill: true,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgb(255, 99, 132)',
+          pointBackgroundColor: 'rgb(255, 99, 132)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgb(255, 99, 132)'
+        }, {
+          label: 'My Second Dataset',
+          data: [28, 48, 40, 19, 96, 27, 100],
+          fill: true,
+          backgroundColor: 'rgba(54, 162, 235, 0.2)',
+          borderColor: 'rgb(54, 162, 235)',
+          pointBackgroundColor: 'rgb(54, 162, 235)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgb(54, 162, 235)'
+        }]
+      }, 
+    options: {
+        elements: {
+          line: {
+            borderWidth: 3
+          }
+        }
+    }
+});
